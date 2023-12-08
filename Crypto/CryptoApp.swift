92243373b4a798cbab8d8e680c7283288d34a14d
Togosling <toga.abdyldaev@gmail.com>
2023-12-08 11:57:15 +0600

    crypto

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Тагай Абдылдаев on 2023/12/8.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
